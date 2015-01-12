0c065cad76d782f164b2bd0031fdfb0c1d28e33d
dv <wdv4758h@gmail.com>
2015-01-12 13:54:49 +0800

    [article] add backup of 'The history of grep'

---
+++ content/html/backup/The history of grep/「grep was a private command of mine for quite a while before i made it public.」 -Ken Thompson — Medium_files/icons.js
@@ -0,0 +1,6 @@
+;(function () {
+  var element = document.createElement('div')
+  element.style.display = 'none'
+  element.innerHTML = '<svg xmlns=\"http://www.w3.org/2000/svg\">\n  <symbol viewBox=\"0 0 22 22\" id=\"svg-logo\">\n    <title>logo</title>\n    <path d=\"M0 17.596V15.33h.655q.258 0 .494-.253t.235-.478V3.93q0-.226-.236-.462t-.495-.236H0V.742h8.12l2.687 9.84h.064L13.6.742h8.186v2.492h-.924q-.226 0-.457.236t-.23.462v10.624q0 .226.23.478t.457.252h.924v2.31h-7.788v-2.31h1.58V4.2h-.065l-3.856 13.396H8.714L4.934 4.2h-.108v11.13h1.622v2.266H.003z\"\n    />\n  </symbol>\n</svg>'
+  document.body.insertBefore(element, document.body.firstChild)
+}());
